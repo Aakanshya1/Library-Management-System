fb72c5048094535bf83693b81e81bbc88d039203
Asus <Asus@DESKTOP-AV2AI2K>
2024-03-15 21:13:53 +0545

    Add project files.

---
+++ LMS/Models/ILibraryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace LMS.Models
+{
+    public interface ILibraryRepository
+    {
+        IEnumerable<Book> AllBooks { get; }
+        void AddBook(Book book);
+        Book? GetBookById(int BookId);
+        void UpdateBook(Book book);
+        void DeleteBook(int BookId);
+    }
+}
---
+++ LMS/ViewModel/LibraryListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LMS.Models;
+
+namespace LMS.ViewModel
+{
+    public class LibraryListViewModel
+    {
+        public IEnumerable<Book> Books { get; }
+
+        public LibraryListViewModel(IEnumerable<Book> books)
+        {
+
+            Books = books;
+        }
+    }
+}
